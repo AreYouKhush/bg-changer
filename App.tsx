4861769da93e2ce2c7dab686ef29291faa87b4e9
AreYouKhush <khushal9587@gmail.com>
2024-01-23 14:48:11 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,97 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import React, {useRef, useState} from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  const [bg, setBg] = useState('white');
+  const interval : any = useRef();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const generateColor = () => {
+    const hexRange = '012345679ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * hexRange.length)];
+    }
+    console.log(color);
+    setBg(color);
+  };
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const partyMode = (party: boolean) => {
+    if (party) {
+      interval.current = setInterval(() => {
+        generateColor();
+      }, 100);
+    } else {
+      clearInterval(interval.current);
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+    <>
+      <StatusBar backgroundColor={bg} />
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          flex: 1,
+          backgroundColor: bg,
+          gap: 20,
+        }}>
+        <TouchableOpacity onPress={generateColor}>
+          <View
+            style={{borderRadius: 18, backgroundColor: 'black', padding: 20, opacity: 0.70}}>
+            <Text
+              style={{
+                color: 'white',
+                textTransform: 'uppercase',
+                fontSize: 20,
+                fontWeight: '600',
+              }}>
+              Press Me!
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            partyMode(true);
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+          <View
+            style={{borderRadius: 10, backgroundColor: 'black', padding: 10, opacity: 0.70}}>
+            <Text
+              style={{
+                color: 'white',
+                textTransform: 'uppercase',
+                fontSize: 12,
+                fontWeight: '600',
+              }}>
+              Start a party
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            partyMode(false);
+          }}
+          style={{
+            position: 'absolute',
+            bottom: 20,
+            right: 20,
+            backgroundColor: 'black',
+            borderRadius: 5,
+          }}>
+          <View>
+            <Text style={{color: 'white', padding: 10}}>Stop</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+const styles = StyleSheet.create({});
